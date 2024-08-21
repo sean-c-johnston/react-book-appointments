99864d4e8cd61e92edf9858f6db7082deb458316
Sean Johnston <sean.johnston@codurance.com>
2024-08-21 16:41:56 +0100

    cleanup test setup

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,33 +1,32 @@
-import React from 'react';
+import React, {act} from 'react';
 import ReactDOM from 'react-dom/client';
 
 import {Appointment} from '../src/Appointment'
-import {act} from "react";
 
 describe("Appointment", () => {
+    let container;
+
+    beforeEach(() => {
+        container = document.createElement('div');
+        document.body.replaceChildren(container);
+    })
+
     it("renders the customer's first name", () => {
         const customer = {firstName: "Sean"};
-        const component = <Appointment customer={customer}/>;
 
-        const container = document.createElement("div");
-        document.body.replaceChildren(container);
-
-        act(() =>
-            ReactDOM.createRoot(container).render(component));
+        render(<Appointment customer={customer}/>);
 
         expect(document.body.textContent).toContain("Sean");
     });
 
     it("renders a different customer's first name", () => {
         const customer = {firstName: "Sophie"};
-        const component = <Appointment customer={customer}/>;
 
-        const container = document.createElement("div");
-        document.body.replaceChildren(container);
-
-        act(() =>
-            ReactDOM.createRoot(container).render(component));
+        render(<Appointment customer={customer}/>);
 
         expect(document.body.textContent).toContain("Sophie");
     });
+
+    const render = component => act(() => ReactDOM.createRoot(container)
+        .render(component));
 });
