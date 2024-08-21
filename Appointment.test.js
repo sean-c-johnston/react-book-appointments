c84c08389748569fb1d99aafc8307312c55859a7
Sean Johnston <sean.johnston@codurance.com>
2024-08-21 15:36:14 +0100

    make test pass

--- Appointment.test.js
+++
@@ -1,5 +0,0 @@
-describe("Appointment", () => {
-    it("renders the customer's first name", () => {
-        expect(document.body.textContent).toContain("Sean");
-    });
-});
---
+++ test/Appointment.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import {Appointment} from '../src/Appointment'
+import {act} from "react";
+
+describe("Appointment", () => {
+    it("renders the customer's first name", () => {
+        const customer = {firstName: "Sean"};
+        const component = <Appointment customer={customer}/>;
+
+        const container = document.createElement("div");
+        document.body.appendChild(container);
+
+        act(() =>
+            ReactDOM.createRoot(container).render(component));
+
+        expect(document.body.textContent).toContain("Sean");
+    });
+});
