e16a22a7efb38eb7d71b69c1be37c6a5cdf54ea3
Sean Johnston <sean.johnston@codurance.com>
2024-08-29 09:41:26 +0100

    Ignore `punycode` deprecation warning in vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,9 @@ export default defineConfig({
     test: {
         setupFiles: ["./test/vitest.setup.ts"],
         environment: "jsdom",
+        env: {
+            NODE_OPTIONS: "--disable-warning=DEP0040"
+        },
         globals: true,
         exclude: ["**/ts-build/**", ...configDefaults.exclude]
     }
