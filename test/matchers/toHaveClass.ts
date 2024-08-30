6f2d59d7535252de8b03cc00c94707176e7f10ca
Sean Johnston <sean.johnston@codurance.com>
2024-08-30 15:22:50 +0100

    tidy

--- test/matchers/toHaveClass.ts
+++ test/matchers/toHaveClass.ts
@@ -1,18 +1,16 @@
 import {matcherHint, printExpected} from "jest-matcher-utils";
 
 export function toHaveClass(received: { className: string }, expected: string) {
-    let pass = received.className.split(" ").includes(expected);
-
     const sourceHint = matcherHint(
         "toHaveClass",
         "element",
         printExpected(expected),
-        {isNot: pass}
+        {isNot: this.isNot}
     );
 
     return {
-        pass: pass,
-        message: () => sourceHint + "\n\n" + "actual:",
+        pass: received.className.split(" ").includes(expected),
+        message: () => sourceHint,
         expected: expected,
         actual: received.className
     };
