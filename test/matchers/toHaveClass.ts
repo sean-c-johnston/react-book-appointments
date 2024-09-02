4c203cd26d8a4f5c41cab5019aeaf827af0ee136
Sean Johnston <sean.johnston@codurance.com>
2024-09-02 08:47:29 +0100

    add more descriptive failure message in toHaveClass matcher

--- test/matchers/toHaveClass.ts
+++ test/matchers/toHaveClass.ts
@@ -1,17 +1,17 @@
-import {matcherHint, printExpected} from "jest-matcher-utils";
+import {matcherHint, printExpected, printReceived} from "jest-matcher-utils";
 
 export function toHaveClass(received: { className: string }, expected: string) {
-    const sourceHint = matcherHint(
+    const sourceHint = () => matcherHint(
         "toHaveClass",
         "element",
         printExpected(expected),
         {isNot: this.isNot}
     );
 
+    const actualTextHint = () => `Actual className: ${printReceived(received.className)}`;
+
     return {
         pass: received.className.split(" ").includes(expected),
-        message: () => sourceHint,
-        expected: expected,
-        actual: received.className
+        message: () => [sourceHint(), actualTextHint()].join("\n\n"),
     };
 }
