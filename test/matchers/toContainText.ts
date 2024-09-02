121a6d124fd35ae5eee17415cc64522e24ac61c0
Sean Johnston <sean.johnston@codurance.com>
2024-09-02 08:59:34 +0100

    add more descritpive message in toContainText matcher

--- test/matchers/toContainText.ts
+++ test/matchers/toContainText.ts
@@ -1,17 +1,17 @@
-import {matcherHint, printExpected} from "jest-matcher-utils";
+import {matcherHint, printExpected, printReceived} from "jest-matcher-utils";
 
 export function toContainText(received: { textContent: string }, expected: string) {
-    const sourceHint = matcherHint(
+    const sourceHint = () => matcherHint(
         "toContainText",
         "element",
         printExpected(expected),
         {isNot: this.isNot}
     );
 
+    const actualHint = () => `Actual text: ${printReceived(received.textContent)}`;
+
     return {
         pass: received.textContent.includes(expected),
-        message: () => sourceHint,
-        expected: expected,
-        actual: received.textContent
+        message: () => [sourceHint(), actualHint()].join("\n\n"),
     };
 }
