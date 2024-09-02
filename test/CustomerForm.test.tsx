805c21893fabcbbc9706e31f410f9940e8841e0b
Sean Johnston <sean.johnston@codurance.com>
2024-09-02 12:03:05 +0100

    cleanup test for form elements

--- test/CustomerForm.test.tsx
+++ test/CustomerForm.test.tsx
@@ -12,27 +12,27 @@ describe("CustomerForm", () => {
 
     it("renders a form", () => {
         render(<CustomerForm original={blankCustomer()}/>);
+
         expect(form()).not.toBeNull();
     });
 
     it("renders the first name in a text box", () => {
         render(<CustomerForm original={blankCustomer()}/>);
 
-        const field = form().elements["firstName"];
-
-        expect(field).not.toBeNull();
-        expect(field.tagName).toEqual("INPUT");
-        expect(field.type).toEqual("text");
+        expect(formField("firstName")).not.toBeNull();
+        expect(formField("firstName").tagName).toEqual("INPUT");
+        expect(formField("firstName").type).toEqual("text");
     });
 
     it("includes the existing first name value", () => {
         const customer = customerWith({ firstName: "Ashley" });
+
         render(<CustomerForm original={customer}/>);
 
-        const field = form().elements["firstName"];
-
-        expect(field.value).toEqual("Ashley");
+        expect(formField("firstName").value).toEqual("Ashley");
     });
 
     const form = () => element<HTMLFormElement>("form");
+
+    const formField = (name: string) => form().elements[name];
 });
